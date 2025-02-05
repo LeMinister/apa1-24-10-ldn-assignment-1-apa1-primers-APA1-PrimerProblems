e5af09638c54ccc67afbef8d60b9a76aa6c1d812
LeMinister <melvin.nguessan@ada.ac.uk>
2025-02-05 09:44:35 +0000

    Melvin primer1.js

--- primer1/primer1.js
+++ primer1/primer1.js
@@ -1,27 +1,61 @@
-function temperatureConversion(temperature, fromScale, toScale){
+function temperatureConversion(temperature, fromScale, toScale) {
+    if (isNaN(temperature)|| temperature== null) {
+        throw new Error('Temperature must be a numeric value');
+        //console.log('Not a valid number');
+        //return null;
+    } else {
+        temperature = parseFloat(temperature); 
+    }
 
-// TODO: Validate the input:
-//        - Check if the temperature is null, undefined, or a non-numeric string. If so, throw an error "Invalid temperature input".
-//        - Convert the temperature to a number.
-//        - Normalise fromScale and toScale to uppercase.
-//        - Check if fromScale and toScale are valid ( e.g. 'C', 'F'). If not, throw an error "Invalid conversion type or input scale".
+    
+    fromScale = fromScale.toLowerCase();
+    toScale = toScale.toLowerCase();
+    
+    //let result=0
+    let result;
 
-// TODO: Define helper functions for the conversions:
-//        - toFahrenheit(celsius): Converts Celsius to Fahrenheit.
-//        - toCelsius(fahrenheit): Converts Fahrenheit to Celsius.
-//        
-// TODO: Implement the conversion logic:
-//        - Use conditional logic to handle different toScale values (e.g. 'C', 'F').
-//        - Within each condition, handle conversions from different fromScale values (e.g. 'C', 'F') to the target toScale.
-//        - Use the helper functions to perform the actual conversions.
+    if (fromScale === toScale) {
+        result = temperature 
+        return result;
+    }
 
+    else if (fromScale === 'c' && toScale === 'f') {
+        result = (temperature * 9 / 5) + 32;
+        return result;
+    }
 
+    else if (fromScale === 'c' && toScale === 'k') {
+        result = temperature + 273.15;
+        return result;
+    }
+ 
+    else if (fromScale === 'f' && toScale === 'c') {
+        result = (temperature - 32) * 5 / 9;
+        return result;
+    }
+    
+    
+    else if (fromScale === 'f' && toScale === 'k') {
+        result = ((temperature - 32) * 5 / 9) + 273.15;
+        return result;
+    }
+    
+    else if (fromScale === 'k' && toScale === 'c') {
+        result = temperature - 273.15;
+        return result;
+    }
+    
+    else if (fromScale === 'k' && toScale === 'f') {
+        result = ((temperature - 273.15) * 9 / 5) + 32;
+        return result;
+    } else {
+        throw new Error('Wrong scale used');
+        //console.log('Wrong scale used');
+        //return null;
+    }
 }
 
-
-
 export default temperatureConversion;
-
 
 
 
